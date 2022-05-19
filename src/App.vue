645b90707b732bbc17933965f2c59b49ff6cd571
Denis Nadarevic <bigboydeku@gmail.com>
2022-05-18 23:49:55 -0400

    Initialize project

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <Navbar></Navbar>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+</template>
+
+<script>
+  import Navbar from './components/Navbar.vue'
+  export default {
+    data() {
+      return {
+
+      }
+    },
+    components: {
+      Navbar
+    }
+  }
+</script>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+body {
+  background-color: #191D32;
+}
+</style>
+
+
---
+++ src/components/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+    <header class="">
+        <nav class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8" aria-label="Top">
+            <div class="w-full py-6 flex items-center justify-between border-b border-indigo-500 lg:border-none">
+                <div class="flex items-center">
+                    <a href="#">
+                    <span class="text-white text-3xl font-bold">DN</span>
+                    </a>
+                    <div class="hidden ml-10 space-x-8 lg:block">
+                        <a href="#" class="text-base font-medium text-white hover:text-indigo-50" key="Solutions">Home</a>
+                        <a href="#" class="text-base font-medium text-white hover:text-indigo-50" key="Pricing">About</a>
+                        <a href="#" class="text-base font-medium text-white hover:text-indigo-50" key="Docs">Projects</a>
+                    </div>
+                </div>
+                <div class="ml-10 space-x-4">
+                    <a href="#" class="inline-block bg-indigo-500 py-2 px-4 border border-transparent rounded-md text-base font-medium text-white hover:bg-opacity-75">Contact</a>
+                    <a href="#" class="inline-block bg-white py-2 px-4 border border-transparent rounded-md text-base font-medium text-indigo-600 hover:bg-indigo-50">Send a Kofi</a>
+                </div>
+            </div>
+            <div class="py-4 flex flex-wrap justify-center space-x-6 lg:hidden">
+                <a href="#" class="text-base font-medium text-white hover:text-indigo-50" key="Solutions">Solutions</a>
+                <a href="#" class="text-base font-medium text-white hover:text-indigo-50" key="Pricing">Pricing</a>
+                <a href="#" class="text-base font-medium text-white hover:text-indigo-50" key="Docs">Docs</a>
+                <a href="#" class="text-base font-medium text-white hover:text-indigo-50" key="Company">Company</a>
+            </div>
+        </nav>
+    </header>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
