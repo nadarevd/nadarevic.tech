072abd08142308f0f98c7baaab4c207fc657430f
Denis Nadarevic <bigboydeku@gmail.com>
2022-06-12 16:35:12 -0400

    added new nav a tags

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -11,6 +11,9 @@
                         <a href="#about" class="text-xs font-light text-[#d5d5d6] hover:text-indigo-50 mx-6" key="Pricing">About</a>
                         <a href="#projects" class="text-xs font-light text-[#d5d5d6] hover:text-indigo-50 mx-6" key="Docs">Projects</a>
                         <a href="#hobbies" class="text-xs font-light text-[#d5d5d6] hover:text-indigo-50 mx-6" key="Docs">Hobbies</a>
+                        <a href="https://www.borderhacks.com/" target="_blank" class="text-xs font-light text-[#d5d5d6] hover:text-indigo-50 mx-6" key="Docs">BorderHacks</a>
+                        <a href="https://winhacks.ca/" target="_blank" class="text-xs font-light text-[#d5d5d6] hover:text-indigo-50 mx-6" key="Docs">WinHacks</a>
+                        <a href="https://github.com/bigboydeku" target="_blank" class="text-xs font-light text-[#d5d5d6] hover:text-indigo-50 mx-6" key="Docs">Github</a>
                         <a href="mailto:bosdenis@hotmail.com" class="has-tooltip inline-block px-4 border border-transparent rounded-md text-base text-white h-full mx-3">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="#d5d5d6" stroke-width="2">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
@@ -48,6 +51,9 @@
                     <a href="#about" @click="navFlag = !navFlag"  class="text-sm font-light py-4 text-[#d5d5d6] hover:text-indigo-50 text-left border-b border-gray-500" key="Pricing">About</a>
                     <a href="#projects" @click="navFlag = !navFlag"  class="text-sm font-light py-4 text-[#d5d5d6] hover:text-indigo-50 text-left border-b border-gray-500" key="Docs">Projects</a>
                     <a href="#hobbies" @click="navFlag = !navFlag"  class="text-sm font-light py-4 text-[#d5d5d6] hover:text-indigo-50 text-left border-b border-gray-500" key="Docs">Hobbies</a>
+                    <a href="https://www.borderhacks.com/" @click="navFlag = !navFlag" class="text-sm font-light py-4 text-[#d5d5d6] hover:text-indigo-50 text-left border-b border-gray-500" key="Docs">BorderHacks</a>
+                    <a href="https://winhacks.ca/" @click="navFlag = !navFlag" class="text-sm font-light py-4 text-[#d5d5d6] hover:text-indigo-50 text-left border-b border-gray-500" key="Docs">WinHacks</a>
+                    <a href="https://github.com/bigboydeku" target="_blank" class="text-sm font-light py-4 text-[#d5d5d6] hover:text-indigo-50 text-left border-b border-gray-500" key="Docs">Github</a>
                     <div class="flex flex-row space-x-4 py-4">
                         <a href="mailto:bosdenis@hotmail.com">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="#d5d5d6" stroke-width="2">
