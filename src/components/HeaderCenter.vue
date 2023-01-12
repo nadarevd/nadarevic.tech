ced87d48389eb2c57cabc5b88c21d57dd6301b0e
Denis Nadarevic <bigboydeku@gmail.com>
2023-01-11 22:51:13 -0500

    changed job status

--- src/components/HeaderCenter.vue
+++ src/components/HeaderCenter.vue
@@ -4,18 +4,18 @@
             <div class="pr-16 sm:text-center sm:px-16">
                 <div class="font-medium text-textGrey">
                     <span class="hidden md:inline font-bold mr-3">
-                    Not for Sale!
+                    For Sale!
                     </span>
                     <span class="inline-flex items-center flex-row">
-                        <p>I am currently working as a UX/UI Designer for </p>
-                        <div class="inline-flex flex-row items-center space-x-1 mx-2">
+                        <p>I am looking to grow as a Front-End Developer & UX/UI Designer! </p>
+                        <!-- <div class="inline-flex flex-row items-center space-x-1 mx-2">
                             <img src="/assets/loadly_icon.svg" class="w-4 h-4" alt="">
                             <a class="text-[#363e7d] hover:underline" href="https://loadly.ca">Loadly</a>
-                        </div>
+                        </div> -->
                     </span>
-                    <span class="block sm:ml-2 sm:inline-block">
+                    <!-- <span class="block sm:ml-2 sm:inline-block">
                         <a href="https://www.loadly.ca/" target="_blank" class="text-textGrey font-bold underline"> Learn more <span aria-hidden="true">&rarr;</span></a>
-                    </span>
+                    </span> -->
                 </div>
             </div>
         </div>
