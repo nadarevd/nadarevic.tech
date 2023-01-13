a6953c2c08de8c565c4454e0f3fa9051e9714264
Denis Nadarevic <bigboydeku@gmail.com>
2023-01-13 15:01:33 -0500

    made it into a downloadable file

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -14,7 +14,7 @@
                         <a href="https://www.borderhacks.com/" target="_blank" class="text-xs font-light text-[#d5d5d6] hover:text-indigo-50 mx-6">BorderHacks</a>
                         <a href="https://winhacks.ca/" target="_blank" class="text-xs font-light text-[#d5d5d6] hover:text-indigo-50 mx-6">WinHacks</a>
                         <a href="https://github.com/bigboydeku" target="_blank" class="text-xs font-light text-[#d5d5d6] hover:text-indigo-50 mx-6">Github</a>
-                        <a href="../../public/assets/files/Resume 2022.pdf" target="_blank" class="text-xs underline text-[#d5d5d6] font-bold hover:text-indigo-50 mx-6">Resume</a>
+                        <a href="../../public/assets/files/Resume 2022.pdf" download="Resume" target="_blank" class="text-xs underline text-[#d5d5d6] font-bold hover:text-indigo-50 mx-6">Resume</a>
                         <a href="mailto:bosdenis@hotmail.com" class="has-tooltip inline-block px-4 border border-transparent rounded-md text-base text-white h-full mx-3">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="#d5d5d6" stroke-width="2">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
